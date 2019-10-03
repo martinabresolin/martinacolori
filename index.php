0c37a0f3e2cc36fa306df58a23232ead84b8d3b3
Martina Bresolin <bresolinmarty@gmail.com>
2019-10-03 17:08:24 +0200

    Modificato intro index

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
-echo "Ciao Lelly Kelly!";
+
+$colore = [
+  "nome" => "Colore Bianco",
+  "code" => "#ffffff",
+  "id" => 1  
+];
+
+//$colore = getColore();
+
 ?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?=$colore['nome']?></h1>
+</body>
+</html>
